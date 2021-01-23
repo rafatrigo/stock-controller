f71875bffe62dd71083be6a5f9d0337ab1b07f60
Rafael Trigo <rafael.trigo.azevedo@gmail.com>
2021-01-23 19:38:00 -0300

    Routes added

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import productsRouter from '@modules/product/infra/http/products.routes';
+import categoriesRouter from '@modules/category/infra/http/categories.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/categories', categoriesRouter);
 
 export default routes;
