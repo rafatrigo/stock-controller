9daf64f145a50c2bf8e321f40bf986d62ea1319a
Rafael Trigo <rafael.trigo.azevedo@gmail.com>
2021-01-23 19:39:29 -0300

    Product - routes added

--- src/modules/product/infra/http/products.routes.ts
+++ src/modules/product/infra/http/products.routes.ts
@@ -1,4 +1,10 @@
 import CreateProductServer from '@modules/product/services/CreateProductService';
+import DecrementProductQuantityService from '@modules/product/services/DecrementProductQuantityService';
+import DeleteProductService from '@modules/product/services/DeleteProductService';
+import FindProductsInCategory from '@modules/product/services/FindProductsInCategoryService';
+import IncrementProductQuantityService from '@modules/product/services/IncrementProductQuantityService';
+import ListProductsServer from '@modules/product/services/ListProductsServer';
+import UpdateProductService from '@modules/product/services/UpdateProductService';
 import { Router } from 'express';
 import { container } from 'tsyringe';
 
@@ -26,7 +32,60 @@ productsRouter.post('/', async (request, response) => {
     category,
   });
 
-  return response.json(product);
+  return response.status(201).json(product);
+});
+
+// delete products
+productsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteProduct = container.resolve(DeleteProductService);
+
+  await deleteProduct.execute(id);
+
+  return response.status(204).send();
+});
+
+// list products
+productsRouter.get('/', async (request, response) => {
+  const listProducts = container.resolve(ListProductsServer);
+
+  const products = await listProducts.execute();
+
+  return response.json(products);
+});
+
+// modify product
+productsRouter.patch('/', async (request, response) => {
+  const newProduct = request.body;
+
+  const update = container.resolve(UpdateProductService);
+
+  const updatedProduct = await update.execute(newProduct);
+
+  return response.json(updatedProduct);
+});
+
+// increment quantity
+productsRouter.put('/:id/increment', async (request, response) => {
+  const { id } = request.params;
+
+  const increment = container.resolve(IncrementProductQuantityService);
+
+  const productIncremented = await increment.execute(id);
+
+  return response.json(productIncremented);
+});
+
+// decrement quantity
+productsRouter.put('/:id/decrement', async (request, response) => {
+  const { id } = request.params;
+
+  const decrement = container.resolve(DecrementProductQuantityService);
+
+  const productDecremented = await decrement.execute(id);
+
+  return response.json(productDecremented);
 });
 
 export default productsRouter;
